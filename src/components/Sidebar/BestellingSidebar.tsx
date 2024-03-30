eeec1b433f155b5de52fed2eef6f528d90fae745
ArneMoens <arne.ad.moens@outlook.com>
2024-03-30 22:36:35 +0100

    <></>?

--- src/components/Sidebar/BestellingSidebar.tsx
+++ src/components/Sidebar/BestellingSidebar.tsx
@@ -30,8 +30,10 @@ function BestellingSidebar() {
   return (
     <div className='bestelling-sidebar'>
       {loading ? (
+      <>
       <div>Loading...</div>
       <div>zonder spinner :(</div>
+      </>
 //           <div
 //               className="animate-spin inline-block size-6 border-[3px] border-current border-t-transparent text-blue-600 rounded-full dark:text-blue-500"
 //               role="status" aria-label="loading">

7b4d7aac2415737289c4f78bd95b5969efa446ce
ArneMoens <arne.ad.moens@outlook.com>
2024-03-30 22:27:17 +0100

    loading spinner

--- src/components/Sidebar/BestellingSidebar.tsx
+++ src/components/Sidebar/BestellingSidebar.tsx
@@ -30,11 +30,13 @@ function BestellingSidebar() {
   return (
     <div className='bestelling-sidebar'>
       {loading ? (
-          <div
-              className="animate-spin inline-block size-6 border-[3px] border-current border-t-transparent text-blue-600 rounded-full dark:text-blue-500"
-              role="status" aria-label="loading">
-            <span className="sr-only">Loading...</span>
-          </div>
+      <div>Loading...</div>
+      <div>zonder spinner :(</div>
+//           <div
+//               className="animate-spin inline-block size-6 border-[3px] border-current border-t-transparent text-blue-600 rounded-full dark:text-blue-500"
+//               role="status" aria-label="loading">
+//             <span className="sr-only">Loading...</span>
+//           </div>
       ) : bestellingItems.length === 0 ? (
           <>
             <div className="bestelling-sidebar-header">
